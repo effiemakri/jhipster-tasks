aa14afe9ab48acb6de9dc483b95b12b118a0eb04
Effie Makri <emakri@telint.eu>
2017-12-12 22:17:04 +0200

    Initial application generated by JHipster-4.12.0

---
+++ src/main/java/eu/telint/tasks/repository/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data JPA repositories.
+ */
+package eu.telint.tasks.repository;
---
+++ src/main/java/eu/telint/tasks/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package eu.telint.tasks.web.rest.vm;
